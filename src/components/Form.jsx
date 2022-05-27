ba80741caa034f9a0c36532d3397fd17118d967d
Rodrigo Barros <brodrigo87@gmail.com>
2022-05-27 12:02:40 -0300

    test

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,7 @@ export default function Form() {
         <Paper style={{ padding: "1em" }}>
             <div style={{ display: "flex", justifyContent: "center" }}>
                 <TextField id="outlined-basic" label="Tarefa" variant="outlined" />
-                <Button variant="text">- Adicionar</Button>
+                <Button variant="text">Adicionar</Button>
             </div>
 
         </Paper>
